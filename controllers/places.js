158e5d5e8656d2fc9f457160fe6782c79a6eba1d
saradonnelly97 <sara.donnelly8@gmail.com>
2023-07-12 14:20:31 -0400

    Jazzed up form page

--- controllers/places.js
+++ controllers/places.js
@@ -22,6 +22,18 @@ router.get('/', (req, res) => {
     res.render('places/index', { places })
 })
 
+router.post('/', (req, res) => {
+    if (req.body.hasGluten === 'on') {
+        req.body.hasGluten = true
+    } else {
+        req.body.hasGluten = false
+    }
+
+    if (!req.body.image) req.body.image = 'https://images.unsplash.com/photo-1534620808146-d33bb39128b2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80'
+
+    Bread.push(req.body)
+    res.redirect('/places')
+})
 
 
 module.exports = router
