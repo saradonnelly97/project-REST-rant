be94af34ad7c2990f5adff8c4836c4b5db9dbcf4
saradonnelly97 <sara.donnelly8@gmail.com>
2023-07-12 20:07:49 -0400

    now the form works

--- controllers/places.js
+++ controllers/places.js
@@ -1,32 +1,32 @@
 const router = require('express').Router()
 
+let places = [{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: '/images/fukuBGA.jpeg'
+  }, {
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: '/images/fukuBGB.jpeg'
+  }]
 router.get('/new', (req, res) => {
     res.render('places/new')
   })  
 
 router.get('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: '/images/fukuBGA.jpeg'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: '/images/fukuBGB.jpeg'
-      }]
 
     res.render('places/index', { places })
 })
 
 router.post('/', (req, res) => {
+    console.log(req.body)
     if (!req.body.pic) {
       // Default image if one is not provided
-      req.body.pic = '/images/joshuatree.jpeg'
-      // https://unsplash.com/photos/-9UJTnXpUXM
+      req.body.pic = 'http://placekitten.com/400/400'
     }
     if (!req.body.city) {
       req.body.city = 'Anytown'
@@ -35,8 +35,8 @@ router.post('/', (req, res) => {
       req.body.state = 'USA'
     }
     places.push(req.body)
-    res.redirect('./places')
+    res.redirect('/places')
   })
 
-
+  
 module.exports = router
