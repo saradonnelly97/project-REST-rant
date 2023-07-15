b997da454c9f0d10ef941811d971cdd5ab1a804b
saradonnelly97 <sara.donnelly8@gmail.com>
2023-07-15 06:59:02 -0400

    Show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -9,6 +9,19 @@ router.get("/", (req, res) => {
     res.render("places/index", { places });
   })
 
+  router.get('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+      res.render('error404')
+    }
+    else if (!places[id]) {
+      res.render('error404')
+    }
+    else {
+      res.render('places/show', { place: places[id], id})
+    }
+  })  
+  
 router.post('/', (req, res) => {
     console.log(req.body)
     if (!req.body.pic) {
